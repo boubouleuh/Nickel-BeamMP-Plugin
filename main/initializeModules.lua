5c2f62a1e904ce75439f3f2f39f2100aece5978c
boubouleuh <adonis@3000fr.com>
2024-06-15 12:13:52 +0000

    modules fix, now need to run modules.sh manually

--- main/initializeModules.lua
+++ main/initializeModules.lua
@@ -16,27 +16,10 @@ function init.initialize()
         return success
     end
 
-    local function installModules()
-        utils.nkprint("Checking Nickel modules", "info")
 
-        local cmd = "luarocks --lua-version 5.3 --tree " .. utils.script_path() .. " install lsqlite3 && luarocks --lua-version 5.3 --tree " .. utils.script_path() .. " install toml && luarocks --lua-version 5.3 --tree " .. utils.script_path() .. " install luasec"
-        os.execute(cmd)
-        utils.nkprint("If there is problem run this command manually while the server is offline (You cant if you are on an hosting solution) : " .. cmd, "warn")
-    end
-
-
-
-    -- Check if LuaRocks is installed
-    local installCmd = "sudo apt install -y libssl-dev cmake libsqlite3-dev build-essential libreadline-dev unzip lua5.3 && curl -R -O https://luarocks.github.io/luarocks/releases/luarocks-3.10.0.tar.gz && tar -zxf luarocks-3.10.0.tar.gz && (cd luarocks-3.10.0 && ./configure) && (cd luarocks-3.10.0 && make) && (cd luarocks-3.10.0 && sudo make install) && curl -R -O https://www.sqlite.org/2024/sqlite-autoconf-3450300.tar.gz && tar -zxf sqlite-autoconf-3450300.tar.gz && (cd sqlite-autoconf-3450300 && ./configure) && (cd sqlite-autoconf-3450300 && make) && (cd sqlite-autoconf-3450300 && sudo make install)"
-
-    if not isLuaRocksInstalled() then
-        os.execute(installCmd)
-        utils.nkprint("If there is problem run this command manually while the server is offline (You cant if you are on an hosting solution) : " .. installCmd, "warn")
-    end
-
-    if not isModuleInstalled("lsqlite3") or not isModuleInstalled("ssl.https") or not isModuleInstalled("toml") then
-        installModules()
-        utils.nkprint("If it didn't work, you may need to uninstall LuaRocks yourself and try again if you already have it installed (sudo apt remove luarocks)", "warn")
+    if not isModuleInstalled("lsqlite3") or not isModuleInstalled("ssl.https") or not isModuleInstalled("toml") or not isLuaRocksInstalled() then
+        utils.nkprint("You dont have the needed modules, please run " .. utils.script_path() .. "modules.sh", "error")
+        exit()
     end
 end
 
---
+++ modules.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Function to check if the script is run as root
+function checkRoot() {
+    if [ "$(id -u)" -ne 0 ]; then
+        echo "This script must be run as root. Please use sudo."
+        exit 1
+    fi
+}
+
+askToInstall() {
+    read -p "The program need you to install : LuaRocks 3.10.0 and dependencies, libssl-dev, sqlite3 3.45, libsqlite3, build-essential, libreadline-dev, unzip, lua5.3 | Do you want to install ? (y/n): " choice
+    case "$choice" in
+        y|Y ) 
+            return 0  # True
+            ;;
+        n|N ) 
+            return 1  # False
+            ;;
+        * ) 
+            askToInstall "$1"  # Repeat the function call
+            ;;
+    esac
+}
+
+# Command to install LuaRocks and its dependencies
+installCmd="apt update && \
+            apt install -y libssl-dev cmake libsqlite3-dev build-essential libreadline-dev unzip lua5.3 && \
+            curl -R -O https://luarocks.github.io/luarocks/releases/luarocks-3.10.0.tar.gz && \
+            tar -zxf luarocks-3.10.0.tar.gz && \
+            (cd luarocks-3.10.0 && ./configure) && \
+            (cd luarocks-3.10.0 && make) && \
+            (cd luarocks-3.10.0 && make install) && \
+            curl -R -O https://www.sqlite.org/2024/sqlite-autoconf-3450300.tar.gz && \
+            tar -zxf sqlite-autoconf-3450300.tar.gz && \
+            (cd sqlite-autoconf-3450300 && ./configure) && \
+            (cd sqlite-autoconf-3450300 && make) && \
+            (cd sqlite-autoconf-3450300 && make install)"
+
+# Function to install required Lua modules
+function installModules() {
+    luarocks --lua-version 5.3 --tree $script_dir install lsqlite3
+    luarocks --lua-version 5.3 --tree $script_dir install toml 
+    luarocks --lua-version 5.3 --tree $script_dir install luasec 
+}
+
+# Check for root privileges
+checkRoot
+
+
+if ! askToInstall; then
+    echo "Install cancelled !"
+    exit
+fi
+
+# Warn user about manual uninstallation if necessary
+echo "If there is a problem, you may need to uninstall LuaRocks manually (sudo apt remove luarocks)."
+
+echo "Installing LuaRocks and dependencies..."
+eval $installCmd
+
+echo "Installing Lua modules..."
+installModules
+
+echo "Installation complete."
