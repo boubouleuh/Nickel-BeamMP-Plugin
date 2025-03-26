a7f138d087fd9b1b6380763fd137d58cab0509fd
boubouleuh <adonis@3000fr.com>
2025-03-26 11:23:33 +0000

    auto update last fix

--- main/updater.lua
+++ main/updater.lua
@@ -10,22 +10,6 @@ local function execute_in_dir(dir, command)
 end
 
 
-local function get_latest_commit(dir)
-    local temp_file = "latest_commit.txt"
-    local command = "cd " .. dir .. " && git rev-parse HEAD > " .. temp_file
-    if os.execute(command) then
-        local file = io.open(dir .. temp_file, "r")
-        if file then
-            local commit_hash = file:read("*l") -- Lire la première ligne
-            file:close()
-            os.remove(dir .. temp_file)
-            print(commit_hash)
-            return commit_hash
-        end
-    end
-    return nil -- Si la commande échoue
-end
-
 -- 📌 Vérifie si le projet est déjà un dépôt Git
 function updater.check()
     local git_check = execute_in_dir(utils.script_path(), "git --version > /dev/null 2>&1")
@@ -34,17 +18,7 @@ function updater.check()
         return
     end
     if FS.Exists(utils.script_path() .. ".git") then
-        local before_pull = get_latest_commit(utils.script_path())
-        MP.Sleep(500)
-        execute_in_dir(utils.script_path(), "git pull origin dev > /dev/null 2>&1")
-        MP.Sleep(500)
-        local after_pull = get_latest_commit(utils.script_path())
-
-        if before_pull == after_pull then
-            utils.nkprint("Already up to date.", "info")
-        else
-            utils.nkprint("Update applied successfully!", "success")
-        end
+        execute_in_dir(utils.script_path(), "git pull origin dev")
     else
         utils.nkprint("This project is not a Git repository. Initializing ...", "warn")
         updater.init_git()
