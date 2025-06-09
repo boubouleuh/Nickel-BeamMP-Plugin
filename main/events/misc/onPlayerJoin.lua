5ca935a294faa13d6bcb96419037ba9cc7f7b115
boubouleuh <adonis@3000fr.com>
2025-06-09 14:27:02 +0000

    maybe fixed join

--- main/events/misc/onPlayerJoin.lua
+++ main/events/misc/onPlayerJoin.lua
@@ -6,8 +6,8 @@ local onPlayerJoin = {}
 function onPlayerJoin.new(managers) 
     function onJoin(id)
         local onlineplayers = MP.GetPlayers()
-        for id, player in pairs(onlineplayers) do
-            interfaceUtils.sendPlayer(id, managers.dbManager, managers.permManager, utils.getPlayerBeamMPID(MP.GetPlayerName(id)))
+        for id2, player in pairs(onlineplayers) do
+            interfaceUtils.sendPlayer(id2, managers.dbManager, managers.permManager, utils.getPlayerBeamMPID(MP.GetPlayerName(id)))
         end
         managers.msgManager:SendMessage(id, managers.cfgManager:GetSetting("misc").join_message, {Role = managers.permManager:GetHighestRole(utils.getPlayerBeamMPID(MP.GetPlayerName(id))).roleName, Player = MP.GetPlayerName(id)})
     end
