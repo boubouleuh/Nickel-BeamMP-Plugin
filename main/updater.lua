0f6285192b0653bee1bd4229a3ca0653218836da
boubouleuh <adonis@3000fr.com>
2026-02-20 00:27:32 +0100

    test updater

--- main/updater.lua
+++ main/updater.lua
@@ -23,32 +23,44 @@ Nickel.Version = Updater.get_git_version()
 function Updater.check(force)
     local path = Utils.script_path()
     local advanced = ConfigManager.GetSetting("advanced")
-    
+    local current_time = 0
+    if FS.Exists(path .. ".git") then
+        local ok, time_out = exec_cmd(path, "git log -1 --format=%ct")
+        if ok then current_time = tonumber(time_out) or 0 end
+    end
     if not FS.Exists(path .. ".git") then
         Utils.nkprint("Initializing Git Repository...", "warn")
-        exec_cmd(path, "git init && git remote add origin https://github.com/boubouleuh/Nickel-BeamMP-Plugin.git")
-        exec_cmd(path, "git fetch origin " .. Updater.target)
-        exec_cmd(path, "git checkout -B " .. Updater.target .. " origin/" .. Updater.target)
+        exec_cmd(path, "git init")
+        exec_cmd(path, "git remote add origin https://github.com/boubouleuh/Nickel-BeamMP-Plugin.git")
+        
+        if advanced.update_type == "tags" then
+            exec_cmd(path, "git fetch --tags")
+            local _, latest_data = exec_cmd(path, "git tag --sort=creatordate --format='%(creatordate:unix) %(refname:short)' | tail -n1")
+            local latest_time_str, latest_name = latest_data:match("(%d+)%s+(.+)")
+            
+            if latest_name then
+                exec_cmd(path, "git reset --hard " .. latest_name)
+                current_time = tonumber(latest_time_str) or 0
+            end
+        else
+            exec_cmd(path, "git fetch origin " .. Updater.target)
+            exec_cmd(path, "git reset --hard origin/" .. Updater.target)
+            exec_cmd(path, "git branch --set-upstream-to=origin/" .. Updater.target .. " " .. Updater.target)
+        end
     end
-
     if not advanced.autoupdate and not force then return end
 
     exec_cmd(path, "git fetch origin --tags")
 
     if advanced.update_type == "tags" then
         local _, latest_data = exec_cmd(path, "git tag --sort=creatordate --format='%(creatordate:unix) %(refname:short)' | tail -n1")
-        local latest_time, latest_name = latest_data:match("(%d+)%s+(.+)")
-        
-        local _, current_time = exec_cmd(path, "git log -1 --format=%ct")
-        
-        latest_time = tonumber(latest_time) or 0
-        current_time = tonumber(current_time) or 0
+        local latest_time_str, latest_name = latest_data:match("(%d+)%s+(.+)")
+        local latest_time = tonumber(latest_time_str) or 0
 
         if latest_name and (latest_time > current_time or force) then
             Utils.nkprint("Update found: " .. latest_name, "info")
-            
             exec_cmd(path, "git stash push -m \"Nickel AutoUpdate Backup\"")
-            local ok, err = exec_cmd(path, "git checkout tags/" .. latest_name)
+            local ok, err = exec_cmd(path, "git reset --hard " .. latest_name)
             
             if not ok then 
                 Utils.nkprint("Update failed: " .. err, "error") 
